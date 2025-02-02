ac06be5bd783c33fef5c28b9941739a5d7be2472
ayanchavand <ayan.chavand15@gmail.com>
2025-02-02 17:02:58 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { motion } from 'motion/react'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center w-screen h-screen'>
+        <h1 className='text-8xl font-extrabold text-center'>BIG BOLD LETTER FOR IMPACT</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
